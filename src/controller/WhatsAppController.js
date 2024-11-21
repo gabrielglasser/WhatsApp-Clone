57695ba68195a96f1a01ef3e36fbafbf36a9b9e5
gabrielglasser <sga91000@gmail.com>
2024-11-20 23:40:10 -0300

    Criando o Prototype

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,11 +1,11 @@
 class WhatsAppController {
 
   constructor() {
-    
+    this.elementsPrototype();
     this.loadElements();
 
   }
-  
+
   //tranformar os ids em camelCase
   loadElements() {
     this.el = {};
@@ -15,6 +15,57 @@ class WhatsAppController {
       this.el[Format.getCamelCase(element.id)] = element;
 
     });
+  }
+
+  elementsPrototype() {
+
+    Element.prototype.hide = function () {
+      this.style.dysplay = 'none';
+      return this;
+    }
+
+    Element.prototype.show = function () {
+      this.style.dysplay = 'block';
+      return this;
+    }
+
+    Element.prototype.toggle = function () {
+      this.style.dysplay = (this.style.dysplay === 'none') ? 'block' : 'none';
+      return this;
+    }
+
+    Element.prototype.on = function (events, fn) {
+      events.split(' ').forEach(event => {
+        this.addEventListener(event, fn)
+      });
+      return this;
+    }
+
+    Element.prototype.css = function (styles) {
+      for (let name in styles) {
+        this.style[name] = styles[name];
+      }
+      return this;
+    }
+
+    Element.prototype.addClass = function (name) {
+      this.classList.add(name);
+      return this;
+    }
+
+    Element.prototype.removeClass = function (name) {
+      this.classList.remove(name);
+      return this;
+    }
+
+    Element.prototype.toggleClass = function (name) {
+      this.classList.toggle(name);
+      return this;
+    }
+
+    Element.prototype.hasClass = function (name) {
+      return this.classList.contains(name);
+    }
   }
 
 }
