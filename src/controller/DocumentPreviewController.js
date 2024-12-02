584330a6fef9e1fda4a34067b2f01aced73675f5
gabrielglasser <sga91000@gmail.com>
2024-12-02 20:18:10 -0300

    Ativando o microfone com API MediaDevices.getUserMedia()

--- src/controller/DocumentPreviewController.js
+++ src/controller/DocumentPreviewController.js
@@ -12,26 +12,57 @@ export class DocumentPreviewController {
         case 'image/jpeg':
         case 'image/png':
         case 'image/gif':
-        case 'image/jpg':
-        let reader = new FileReader();
-        reader.onload = (e) => {
-          resolve(reader.result);
-        }
-        reader.onerror = (e) => {
-          reject({
-            src: reader.result,
-            info: this._file.name
-          });
-        }
-        reader.readAsDataURL(this._file);
+          reader.onload = (e) => {
+            resolve(reader.result);
+          }
+          reader.onerror = (e) => {
+            reject({
+              src: reader.result,
+              info: this._file.name
+            });
+          }
+          reader.readAsDataURL(this._file);
           break;
 
-          case 'application/pdf':
+        case 'application/pdf':
+          reader.onload = (e) => {
 
+            pdfjsLib.getDocument(new Uint8Array(reader.result)).then(pdf => {
+
+              pdf.getPage(1).then(page => {
+
+                let viewport = page.getViewport(1);
+                let canvas = document.createElement('canvas');
+                let canvasContext = canvas.getContext('2d');
+
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+                page.render({
+                  canvasContext,
+                  viewport: viewport
+                }).then(() => {
+                  let _resolve = (pdf.numPages > 1) ? 'resolse' : '';
+
+                  resolve(src = canvas.toDataURL(),
+                  info = `${pdf.numPages} páginas${_resolve}`);
+                }).catch(err => {
+                  reject(err);
+                });
+
+              }).catch(err => {
+                reject(err);
+              });
+
+            }).catch(err => {
+              reject(err);
+            });
+
+          }
+          reader.readAsArrayBuffer(this._file);
           break;
 
-          default:
-            reject();
+        default:
+          reject();
           break;
 
       }
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -211,6 +211,9 @@ export default class WhatsAppController {
 
     this.el.inputDocument.on('change', e => {
       if (this.el.inputDocument.files.length) {
+        this.el.panelDocumentPreview.css({
+          'height': '1%'
+        })
         let file = this.el.inputDocument.files[0];
 
         this._documentPreviewController = new DocumentPreviewController(file);
@@ -222,8 +225,15 @@ export default class WhatsAppController {
           this.el.imagePanelDocumentPreview.show();
           this.el.filePanelDocumentPreview.hide();
 
+          this.el.panelDocumentPreview.css({
+            'height': 'calc(100% - 120px)'
+          })
+
         }).catch(err => {
-          console.log(file.type)
+          this.el.panelDocumentPreview.css({
+            'height': 'calc(100% - 120px)'
+          })
+
           switch (file.type) {
             case 'application/vnd.ms-excel':
             case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
