b4a1469a3b2883584af71babf59d78a1fe0a7c93
gabrielglasser <sga91000@gmail.com>
2024-12-02 22:37:23 -0300

    Gravando áudio capturado pelo microfone com MediaRecorder

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -5,27 +5,76 @@ export class MicrophoneController extends ClassEvent {
   constructor() {
     super();
 
+    this._mimeType = 'audio/webm';
+
+    this._available = false;
+
     navigator.mediaDevices.getUserMedia({
       audio: true
     }).then(stream => {
+      this._available = true;
+
       this._stream = stream;
 
-      let audio = new Audio();
-      audio.src = URL.createObjectURL(stream);
-
-      audio.play();
-
-      this.trigger('play', audio);
+      this.trigger('ready', this._stream);
     }).catch(err => {
       console.log(err);
     });
 
   }
 
+  isAvailable() {
+    return this._available;
+  }
+
   stop() {
     this._stream.getTracks().forEach(track => {
       track.stop();
     });
+  }
+
+  startRecorder() {
+
+    if (this.isAvailable()) {
+
+      this._mediaRecorder = new MediaRecorder(this._stream, {
+        mimeType: this._mimeType
+      })
+
+      this._recordedChunks = [];
+
+      this._mediaRecorder.addEventListener('dataavailable', event => {
+        if (event.data && event.data.size > 0) {
+          this._recordedChunks.push(event.data);
+        }
+      });
+
+      this._mediaRecorder.addEventListener('stop', e => {
+
+        let blob = new Blob(this._recordedChunks, { type: this._mimeType });
+
+        let filename = `rec${Date.now()}.webm`;
+
+        let file = new File([blob], filename, {
+          type: this._mimeType,
+          lastModified: Date.now()
+        });
+
+      })
+      this._mediaRecorder.start();
+    }
+
+  }
+
+  stopRecorder() {
+
+    if (this.isAvailable()) {
+
+      this._mediaRecorder.stop();
+      this.stop();
+
+    }
+
   }
 
 }
