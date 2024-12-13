64992adf4f17045faf57921bd2b272380f15f015
gabrielglasser <sga91000@gmail.com>
2024-12-13 16:45:10 -0300

    Enviando foto como anexo

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -423,7 +423,52 @@ export default class WhatsAppController {
       this.el.btnReshootPanelCamera.hide();
     });
 
-    this.el.btnSendPicture.on("click", (e) => {});
+    this.el.btnSendPicture.on("click", (e) => {
+      this.el.btnSendPicture.disabled = true;
+      let regex = /^data:(.+);base64,(.*)$/;
+      let result = this.el.pictureCamera.src.match(regex);
+      let mimeType = result[1];
+      let ext = mimeType.split("/")[1];
+      let filename = `camera${Date.now()}.${ext}`;
+
+      let picture = new Image();
+      picture.src = this.el.pictureCamera.src;
+      picture.onload = () => {
+        let canvas = document.createElement("canvas");
+        let context = canvas.getContext("2d");
+        canvas.width = picture.width;
+        canvas.height = picture.height;
+        context.transform(picture.width, 0);
+        context.scale(-1, 1);
+        context.drawImage(picture, 0, 0, canvas.width, canvas.height);
+
+        fetch(canvas.toDataURL(mimeType))
+          .then((res) => {
+            return res.arrayBuffer();
+          })
+          .then((buffer) => {
+            return new File([buffer], filename, { type: mimeType });
+          })
+          .then((file) => {
+            Message.sendImage(
+              this._contactActive.chatId,
+              this._user.email,
+              file
+            );
+
+            this.el.btnSendPicture.diseabled = false;
+
+            this.closeAllMainPanel();
+            this._camera.stop();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.containertakePicture.show();
+            this.el.containerSendPicture.hide();
+            this.el.panelMessagesContainer.show();
+          });
+      };
+    });
 
     this.el.btnAttachDocument.on("click", (e) => {
       this.closeAllMainPanel();
